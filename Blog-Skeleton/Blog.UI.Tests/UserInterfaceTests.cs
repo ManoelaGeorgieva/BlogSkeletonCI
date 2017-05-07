67b94ece95a874b09e4f81c0328f3993b33d35de
ManoelaGeorgieva <manoelageorgieva@gmail.com>
2017-05-07 21:52:35 +0300

    nunit 2 6 3

--- Blog-Skeleton/Blog.UI.Tests/UserInterfaceTests.cs
+++ Blog-Skeleton/Blog.UI.Tests/UserInterfaceTests.cs
@@ -22,14 +22,14 @@ namespace Blog.UI.Tests
             Assert.AreEqual("List - My ASP.NET Application", driver.Title);
         }
 
-        [Test]
-        public void Register()
-        {
-            IWebDriver driver = BrowserHost.Instance.Application.Browser;
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
-            driver.FindElement(By.Id("registerLink")).Click();
-            Assert.AreEqual("Register", driver.FindElement(By.XPath("/html/body/div[2]/div/div/h2")).Text);
-        }
+        //[Test]
+        //public void Register()
+        //{
+        //    IWebDriver driver = BrowserHost.Instance.Application.Browser;
+        //    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+        //    driver.FindElement(By.Id("registerLink")).Click();
+        //    Assert.AreEqual("Register", driver.FindElement(By.XPath("/html/body/div[2]/div/div/h2")).Text);
+        //}
 
 
         //[Test]

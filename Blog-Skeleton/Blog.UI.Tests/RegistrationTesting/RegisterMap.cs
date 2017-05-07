7e289cc78caacac7315f2d649376238069b86fa7
ManoelaGeorgieva <manoelageorgieva@gmail.com>
2017-05-07 21:22:57 +0300

    39 both places

---
+++ Blog-Skeleton/Blog.UI.Tests/RegistrationTesting/RegisterMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+
+namespace Blog.UI.Tests.RegistrationTesting
+{
+   public class RegisterMap
+    {
+        private IWebDriver driver;
+
+        public IWebElement Email { get
+            { return driver.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[2]/label")); }
+        }
+
+        public IWebElement FullName
+        {
+            get
+            { return driver.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[3]/label")); }
+        }
+    }
+}
